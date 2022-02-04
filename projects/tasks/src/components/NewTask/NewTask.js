e9393826ee1d5aac61e2c8b533f015adb42a83c0
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-02-04 06:40:11 -0700

    edited new task to use hook

--- projects/tasks/src/components/NewTask/NewTask.js
+++ projects/tasks/src/components/NewTask/NewTask.js
@@ -1,15 +1,29 @@
 import useHttp from "../../hooks/use-Http";
-
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
 
 const NewTask = (props) => {
-  const { isLoading, error, requestHandler } = useHttp(true);
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
 
-  const enterTaskHandler = async (taskText) => {
-    const createdTask = await requestHandler(taskText);
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
 
     props.onAddTask(createdTask);
+  };
+
+  const enterTaskHandler = async (taskText) => {
+    sendTaskRequest(
+      {
+        url: "https://test-project-eb0c5-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: { text: taskText },
+      },
+      createTask.bind(null, taskText)
+    );
   };
 
   return (
---
+++ projects/tasks/src/components/NewTask/NewTask2.js
@@ -0,0 +1,23 @@
+import useHttp from "../../hooks/use-Http2";
+
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+
+const NewTask = (props) => {
+  const { isLoading, error, requestHandler } = useHttp(true);
+
+  const enterTaskHandler = async (taskText) => {
+    const createdTask = await requestHandler(taskText);
+
+    props.onAddTask(createdTask);
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
